8fd39de78c3e6b803ecc2145a15197d3d430dc8e
Musthak <musthakcool55@gmail.com>
2026-03-07 21:20:20 +0530

    Videos App velai ellame mudichitten next other apps create panni adhukkuriya velahal thaan irukku, in shaa allah nalaki pakkalam

--- videos/urls.py
+++ videos/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import VideoListView, VideoDetailView
+from .views import VideoListView, VideoDetailView, VideoUpdateView, VideoDeleteView, VideoCreateView
 
 urlpatterns = [
     path('', VideoListView.as_view(), name='video_list'),
     path('<int:pk>/', VideoDetailView.as_view(), name='video_detail'),
+    path('<int:pk>/update/', VideoUpdateView.as_view(), name='video_update'),
+    path('<int:pk>/delete/', VideoDeleteView.as_view(), name='video_delete'),
+    path('create/', VideoCreateView.as_view(), name='video_create'),
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import ListView, DetailView
-from .models import *
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Video
+
+
 class VideoListView(ListView):
     model = Video
     template_name = "videos/video_list.html"
@@ -9,7 +13,56 @@ class VideoListView(ListView):
     ordering = ['-uploaded_at']
     paginate_by = 10
 
+
 class VideoDetailView(DetailView):
     model = Video
     template_name = "videos/video_detail.html"
     context_object_name = "video"
+
+
+class VideoCreateView(LoginRequiredMixin, CreateView):
+    model = Video
+    template_name = "videos/video_create.html"
+
+    fields = [
+        'title',
+        'description',
+        'video_file',
+        'thumbnail',
+    ]
+
+    def form_valid(self, form):
+        form.instance.uploader = self.request.user
+        return super().form_valid(form)
+
+
+class VideoUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Video
+    template_name = "videos/video_update.html"
+
+    fields = [
+        'title',
+        'description',
+        'video_file',
+        'thumbnail',
+    ]
+
+    def form_valid(self, form):
+        form.instance.uploader = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        video = self.get_object()
+        return self.request.user == video.uploader
+
+
+class VideoDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Video
+    template_name = "videos/video_delete.html"
+    context_object_name = "video"
+
+    success_url = reverse_lazy("video_list")
+
+    def test_func(self):
+        video = self.get_object()
+        return self.request.user == video.uploader
