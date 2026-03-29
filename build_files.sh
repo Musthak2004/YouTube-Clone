443f48b673a6256555ce5f7737126cfacd0ca587
Musthak <musthakcool55@gmail.com>
2026-03-29 20:10:46 +0530

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+python -m venv .vercel-venv
+source .vercel-venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput
